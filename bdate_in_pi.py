454a3257a8033514e2684c2bd396a1511e363d2b
Sanan <74050360+sananqsh@users.noreply.github.com>
2022-05-02 13:47:43 -0700

    Update bdate_in_pi.py

--- bdate_in_pi.py
+++ bdate_in_pi.py
@@ -1,33 +1,47 @@
 # Idea of this program originated from Python Crash Course Book
 import sys
-
-if (sys.argv[1] == '10'):
-    file_path = 'Pi10DP.txt'
-elif (sys.argv[1] == '100'):
-    file_path = 'Pi100DP.txt'
-elif (sys.argv[1] == '1000'):
-    file_path = 'Pi1KDP.txt'
-elif (sys.argv[1] == '10000'):
-    file_path = 'Pi10KDP.txt'
-elif (sys.argv[1] == '100000'):
-    file_path = 'Pi100KDP.txt'
-elif (sys.argv[1] == '1000000'):
+if len(sys.argv) == 1:
     file_path = 'Pi1MDP.txt'
-elif (sys.argv[1] == '1250000'):
-    file_path = 'Pi125MDP.txt'
+    accuracy = 1000000
 else:
-    print("Invalid Input!")
-    exit()
+    if (sys.argv[1] == '10'):
+        file_path = 'Pi10DP.txt'
+        accuracy = 10
+    elif (sys.argv[1] == '100'):
+        file_path = 'Pi100DP.txt'
+        accuracy = 100
+    elif (sys.argv[1] == '1000'):
+        file_path = 'Pi1KDP.txt'
+        accuracy = 1000
+    elif (sys.argv[1] == '10000'):
+        file_path = 'Pi10KDP.txt'
+        accuracy = 10000
+    elif (sys.argv[1] == '100000'):
+        file_path = 'Pi100KDP.txt'
+        accuracy = 100000
+    elif (sys.argv[1] == '1000000'):
+        file_path = 'Pi1MDP.txt'
+        accuracy = 1000000
+    elif (sys.argv[1] == '1250000'):
+        file_path = 'Pi125MDP.txt'
+        accuracy = 1250000
+    else:
+        print("Invalid Input!")
+        exit()
 
-with open(file_path) as pi:
-    lines = pi.readlines()
-    bdate = input("Enter your birthday(mmddyy): ")
+try:
+    with open(file_path) as pi:
+        lines = pi.readlines()
+        bdate = input("Enter your birthday(mmddyy): ")
+except FileNotFoundError:
+    print(f"file '{file_path}' does not exist!")
 
-all_pi = "3"
+pi_str = "3"
 for line in lines:
-    all_pi += line[:-1]
+    pi_str += line[:-1]
 
-if bdate in all_pi:
-        print("Yea!")
+found_dig = pi_str.find(bdate)
+if bdate in pi_str:
+        print(f"Your birthday appears in the digit:{found_dig} in Pi number!")
 else:
-    print("Nah!")
+    print(f"Nah! Your birthday does not apear in first {accuracy} digits of Pi")
